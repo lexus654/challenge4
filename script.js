f560cd2493c76496406a258dc4a44cc6b1633518
lexus654 <ronald.laz@tup.edu.ph>
2021-09-08 16:15:11 +0800

    Figure out drop down menu

---
+++ script.js
@@ -0,0 +1,7 @@
+const rotateButton1 = document.querySelector("[data-rotate1]");
+const rotateButton2 = document.querySelector("[data-rotate2]");
+const rotateButton3 = document.querySelector("[data-rotate3]");
+
+rotateButton1.addEventListener("click", function () {
+  console.log(`hello`);
+});
