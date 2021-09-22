0856eda8795b4365d48420ae0b7fe88f8ac09a89
lexus654 <ronald.laz@tup.edu.ph>
2021-09-22 14:53:56 +0800

    Pseudo COde added on Javascript

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ window.addEventListener("resize", function () {
   console.log(txt);
   imageChanger();
 });
+
+// ON DESKTOP VIEW
+// TODO Nav bar
+
+// 1. on focus the list should be underlines
+
+// 2. Open Modal
+
+// 3. Once the modal list is hovered text-color will change
+
+// on blur close modal
+
+//  ON MOBILE
+// 1. on click the hamburger menu will become X
+
+//2 POP UP SCREEN
+
+// WHILE POP UP SCREENTRUE
+// // 1. on focus the list should show the open modal FUNCTION
+
+// 1.1 the size of pop up screen should adjust accordingly
+
+// 2. Open Modal
+
+// on blur close modal
+
+// on click again the hamburger menu is close
+// POP UP SCREEN DISPLAY NONE
